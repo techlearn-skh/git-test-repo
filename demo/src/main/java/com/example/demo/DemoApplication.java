4bed6fefc0430a786251ef92db0ceafa66fd576c
kamal <skkamal2java@gmail.com>
2026-09-08 22:27:40 +0530

    SOP added

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Welcome from main branch.....");
+		System.out.println("Welcome from main branch.....2nd commit");
 	}
 
 }
